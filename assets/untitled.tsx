cd5f4de8eaa17a2d49e10fa28126ef54c40f3039
AntiPap <p14papa@ionio.gr>
2017-11-08 17:04:25 +0200

    Delete untitled.tsx

--- assets/untitled.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="pacman-tiles" tilewidth="16" tileheight="16" tilecount="48" columns="16">
- <image source="pacman-tiles.png" width="256" height="49"/>
-</tileset>
