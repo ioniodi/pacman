506c9dcb93a03a09fd20b588a905099d07ec1142
George Mantellos <geocfuone@gmail.com>
2017-11-25 18:10:30 +0200

    Update GameMenu.js

--- assets/states/GameMenu.js
+++ assets/states/GameMenu.js
@@ -1,1 +1,21 @@
 var GameMenu = function () {};
+
+GameMenu.prototype = {
+  preload: function () {
+  },
+
+  create: function () {
+    game.add.sprite(0, 0, 'menu-bg');
+    game.add.existing(this.titleText);
+  },
+  
+  init: function () {
+    this.titleText = game.make.text(game.world.centerX, 100, "Game Title", {
+      font: 'bold 60pt TheMinion',
+      fill: '#FDFFB5',
+      align: 'center'
+    });
+    this.titleText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
+    this.titleText.anchor.set(0.5);
+  },
+};
