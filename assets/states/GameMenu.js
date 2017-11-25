ca57d5a8435d5fdeedff21178f1e5f34364abd55
George Mantellos <geocfuone@gmail.com>
2017-11-25 18:23:53 +0200

    Update GameMenu.js

--- assets/states/GameMenu.js
+++ assets/states/GameMenu.js
@@ -1,14 +1,20 @@
-var GameMenu = function () {};
+var GameMenu = function() {};
 
 GameMenu.prototype = {
-  preload: function () {
+
+  addMenuOption: function(text, callback) {
+    var txt = game.add.text(30, (this.optionCount * 80) + 200, text, style.navitem.default);
+    txt.inputEnabled = true;
+    txt.events.onInputUp.add(callback);
+    txt.events.onInputOver.add(function (target) {
+      target.setStyle(style.navitem.hover);
+    });
+    txt.events.onInputOut.add(function (target) {
+      target.setStyle(style.navitem.default);
+    });
+    this.optionCount ++;
   },
 
-  create: function () {
-    game.add.sprite(0, 0, 'menu-bg');
-    game.add.existing(this.titleText);
-  },
-  
   init: function () {
     this.titleText = game.make.text(game.world.centerX, 100, "Game Title", {
       font: 'bold 60pt TheMinion',
@@ -17,5 +23,23 @@ GameMenu.prototype = {
     });
     this.titleText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
     this.titleText.anchor.set(0.5);
+    this.optionCount = 1;
   },
+
+  create: function () {
+    game.stage.disableVisibilityChange = true;
+
+    game.add.sprite(0, 0, 'menu-bg');
+    game.add.existing(this.titleText);
+
+    this.addMenuOption('Start', function () {
+      console.log('You clicked Start!');
+    });
+    this.addMenuOption('Options', function () {
+      console.log('You clicked Options!');
+    });
+    this.addMenuOption('Credits', function () {
+      console.log('You clicked Credits!');
+    });
+  }
 };
