d8c41e9a648d5eb15bb8d8e26101e4b50a8c7235
George Mantellos <geocfuone@gmail.com>
2017-11-26 23:18:59 +0200

    Update MainMenu.js

--- assets/states/MainMenu.js
+++ assets/states/MainMenu.js
@@ -1,22 +1,21 @@
 var MainMenu = function(game){}
  
 MainMenu.prototype = {
-  	create: function(){
-		var title = this.game.add.sprite(160, 160, "title");
-		title.anchor.setTo(0.5,0.5);
+    create: function(){
+         var title = this.game.add.sprite(160, 160, "title");
+         title.anchor.setTo(0.5,0.5);
+         var play_level1 = this.game.add.button(160, 320, "play", this.Level1, this);
+         play_level1.anchor.setTo(0.5,0.5);
         
-		var play_level1 = this.game.add.button(160, 320, "play", this.Level1, this);
-		play_level1.anchor.setTo(0.5,0.5);
-        
-        var level = this.game.add.button(180, 320, "level", this.Levels, this);
-		play_level2.anchor.setTo(0.5,0.5);
-	},
-    
-	Level1: function(){
-		this.game.state.start("Level1");
-	},
+         var level = this.game.add.button(180, 320, "level", this.Levels, this);
+         play_level2.anchor.setTo(0.5,0.5);
+    },
+
+    Level1: function(){
+         this.game.state.start("Level1");
+    },
     
     Levels: function(){
-		this.game.state.start("Level2");
-	}
+         this.game.state.start("Level2");
+    }
 }
