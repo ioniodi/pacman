551cb166159c45758c65bce741e3b054432b8391
George Mantellos <geocfuone@gmail.com>
2017-11-26 18:20:48 +0200

    Update Preloader.js

--- assets/states/Preloader.js
+++ assets/states/Preloader.js
@@ -1,19 +1,1 @@
-var Game = [];
 
-Game.Loading = function (game) {
-
-};
-
-Game.Loading.prototype = {
-    init: function () {
-        this.stage.disableVisibilityChange = true;
-    }
-    
-    preload: function () {
-        this.load.image('preloaderbar', 'assets/');
-    }
-    
-    create: function () {
-        this.state.start('Preloader');
-    }
-}
