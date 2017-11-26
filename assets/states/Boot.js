1cde5c508e2a816f3092d5306ee3cf65964442a7
George Mantellos <geocfuone@gmail.com>
2017-11-27 01:19:57 +0200

    Update and rename boot.js to Boot.js

--- assets/states/Boot.js
+++ assets/states/Boot.js
@@ -1,10 +1,10 @@
-var boot = function(game){
+var boot = function(game) {
 	console.log("%cStarting my awesome game", "color:white; background:red");
 };
   
 boot.prototype = {
-	preload: function(){
-          this.game.load.image("loading","assets/loading.png"); 
+	preload: function() {
+          this.game.load.image('loading','assets/loading.png'); 
 	},
   	create: function(){
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
