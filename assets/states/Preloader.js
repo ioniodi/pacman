9b3715a0daacdb54b0f496de5fb8ec9bdbc44f45
George Mantellos <geocfuone@gmail.com>
2017-11-26 19:46:05 +0200

    Update Preloader.js

--- assets/states/Preloader.js
+++ assets/states/Preloader.js
@@ -1,8 +1,15 @@
 Game.Preloader = function (game) {
-    this.preloadBar = null;
+    this.loadingBar = null;
 };
 
 Game.Preloader.prototype = {
+    preload: function () {
+        this.loadingBar = this.add.sprite(this.world.centerX, this.world.centerY, 'Loading');
+        this.loadingBar.anchor.setTO(0.5, 0.5);
+        this.time.advancedTiming = true;
+        this.load.setPreloadSprite(this.loadingBar);
+    }
+    
     create: function() {
         game.state.start('Level1');
     }
