3cf41a37b9b67dff5ee0c4cb6f6f49073b38ad99
George Mantellos <geocfuone@gmail.com>
2017-11-26 22:28:02 +0200

    Update Preload.js

--- assets/states/Preload.js
+++ assets/states/Preload.js
@@ -2,9 +2,9 @@ var Preload = function(game){}
  
 Preload.prototype = {
 	preload: function(){ 
-		this.game.load.image("title","assets/states/buttons/title.png");
-		this.game.load.image("play","assets/states/buttons/play.png");
-		this.game.load.image("level","assets/states/buttons/level.png");
+		this.game.load.image('title','assets/states/buttons/title.png');
+		this.game.load.image('play','assets/states/buttons/play.png');
+		this.game.load.image('level','assets/states/buttons/level.png');
 	},
   	create: function(){
 		console.log('Preload goes to MainMenu');
