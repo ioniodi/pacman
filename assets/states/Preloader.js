687d5c324d00a3acc6308eec752baf21c7b08682
George Mantellos <geocfuone@gmail.com>
2017-11-26 19:57:18 +0200

    Update Preloader.js

--- assets/states/Preloader.js
+++ assets/states/Preloader.js
@@ -4,8 +4,7 @@ Game.Preloader = function (game) {
 
 Game.Preloader.prototype = {
     preload: function () {
-        this.load.image('Loading', 'assets/states/loading.png');
-        this.loadingBar = this.add.sprite(this.world.centerX, this.world.centerY, 'Loading');
+        this.loadingBar = this.add.sprite(180, 300, 'loading');
         this.loadingBar.anchor.setTO(0.5, 0.5);
         this.time.advancedTiming = true;
         this.load.setPreloadSprite(this.loadingBar);
