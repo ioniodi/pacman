e469b5a7b178440a87436bd1710ca78793d270e0
George Mantellos <geocfuone@gmail.com>
2017-11-27 02:55:05 +0200

    Update Level_pick.js

--- assets/states/Level_pick.js
+++ assets/states/Level_pick.js
@@ -1,1 +1,15 @@
+var Level_pick = function (game) {}
 
+Level_pick.prototype = {
+  	create: function(){
+      console.log('%cLevel_pick -> create intitialized', 'color:white; background:red');
+      var Title = this.game.add.sprite(160, 160, 'title');
+      Title.anchor.setTo(0.5, 0.5);
+      var level2 = this.game.add.button(200, 300, 'level', this.Level2, this);
+      level2.anchor.setTo(0.5, 0.5);
+	},
+	
+  Level2: function () {
+		this.game.state.start('Level2');
+	}
+}
