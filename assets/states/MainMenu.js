d0907abe669032cff0229dc5303db67286d3a22a
George Mantellos <geocfuone@gmail.com>
2017-11-27 03:05:53 +0200

    Update MainMenu.js

--- assets/states/MainMenu.js
+++ assets/states/MainMenu.js
@@ -8,7 +8,7 @@ MainMenu.prototype = {
 		var play = this.game.add.button(90, 320, 'play', this.Level1, this);
 		play.anchor.setTo(0.5, 0.5);
         var level2 = this.game.add.button(210, 320, 'level', this.Level2, this);
-		level.anchor.setTo(0.5, 0.5);
+		level2.anchor.setTo(0.5, 0.5);
 	},
 	Level1: function () {
 		this.game.state.start('Level1');
