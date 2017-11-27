a8c2fa70354938fd07a1b91d8b3e6714d15ed858
geocfu <geocfuone@gmail.com>
2017-11-27 20:57:31 +0200

    centered main menu; repositioned buttons

--- assets/states/MainMenu.js
+++ assets/states/MainMenu.js
@@ -3,19 +3,19 @@ var MainMenu = function (game) {}
 MainMenu.prototype = {
   	create: function(){
 		console.log('%cMainMenu -> create intitialized', 'color:white; background:red');
-		var Title = this.game.add.sprite(160, 160, 'title');
+		var Title = this.game.add.sprite(this.world.centerX, this.world.centerY-100, 'title');
 		Title.anchor.setTo(0.5, 0.5);
-		
-		var play = this.game.add.button(90, 320, 'play', this.Level1, this);
+
+		var play = this.game.add.button(this.world.centerX, this.world.centerY, 'play', this.Level1, this);
 		play.anchor.setTo(0.5, 0.5);
-        
-		var level2 = this.game.add.button(210, 320, 'level2', this.Level2, this);
+
+		var level2 = this.game.add.button(this.world.centerX, this.world.centerY + 55, 'level2', this.Level2, this);
 		level2.anchor.setTo(0.5, 0.5);
 	},
 	Level1: function () {
 		this.game.state.start('Level2');
 	},
-     
+
 	level2: function () {
 		this.game.state.start('Level2');
 	}
--- assets/states/Preload.js
+++ assets/states/Preload.js
@@ -1,15 +1,15 @@
 var Preload = function (game) {}
 
 Preload.prototype = {
-	preload: function() { 
-        var loadingBar = this.add.sprite(160, 240, 'loading');
-        loadingBar.anchor.setTo(0.5,0.5);
+	preload: function() {
+        var loadingBar = this.add.sprite(this.world.centerX, this.world.centerY, 'loading');
+        loadingBar.anchor.setTo(0.5, 0.5);
         this.load.setPreloadSprite(loadingBar);
         this.game.load.image('title', 'assets/states/buttons/title.png');
 	    this.game.load.image('play', 'assets/states/buttons/play.png');
 	    this.game.load.image('level2', 'assets/states/buttons/level.png');
 	},
-  	
+
 	create: function(){
         console.log('%cPreload -> create intitialized', 'color:white; background:red');
 		this.game.state.start('MainMenu');
