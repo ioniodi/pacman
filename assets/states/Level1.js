5a2f4255d4dcd3a231696a951e9aea49e5d90e48
George Mantellos <geocfuone@gmail.com>
2017-11-27 02:13:36 +0200

    Update Level1.js

--- assets/states/Level1.js
+++ assets/states/Level1.js
@@ -295,7 +295,7 @@ Pacman.prototype = {
       }
 
       time = this.game.time.totalElapsedSeconds()|0;
-      time_text.text = 'Time: ' + time + ' seconds';
+      time_text = 'Time: ' + time + ' seconds';
 
       if (knife_eaten == 0) {
           if (this.pacman.overlap(this.knife)) {
